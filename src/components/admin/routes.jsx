90c1b00bc404c72853bcce188f98386b00a2a447
ab <ab@ab.ab>
2020-06-30 22:33:57 +0300

    forgot () in render

--- src/components/admin/routes.jsx
+++ src/components/admin/routes.jsx
@@ -5,7 +5,7 @@ import { render } from 'react-dom';
 
 const routes = () => (
   <Switch>
-    <Route exact path="/adminhome" render={<h1>adminhome</h1>} />
+    <Route exact path="/adminhome" render={() => <h1>adminhome</h1>} />
     <Route exact path="/" render={() => <h1>admin root</h1>} />
 
   </Switch>
