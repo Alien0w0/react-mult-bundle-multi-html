123b50b34e3095bd25d37fbd582169fedf533bc6
ab <ab@ab.ab>
2020-06-30 21:31:46 +0300

    div to h1

--- src/components/user/routes.jsx
+++ src/components/user/routes.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const routes = () => (
   <Switch>
-      <Route exact path="/" render={() => <div>user root</div>} />
+      <Route exact path="/" render={() => <h1>user root</h1>} />
       <Route exact path="/userhome" render={() => <h1>user home</h1>} />
   </Switch>
 );
